6691584c63eb340f3ff172010618f31e956be2ac
Admin <channingwang2018@gmail.com>
2023-09-06 23:38:44 +0800

    set baseUrl and make the package public

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,12 +14,13 @@ const config = {
   url: 'https://channingwang2018.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/website/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'ChanningWang2018', // Usually your GitHub org/user name.
   projectName: 'website', // Usually your repo name.
+  trailingSlash: false, // It is recommended to set a trailingSlash config (true or false, not undefined).
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
