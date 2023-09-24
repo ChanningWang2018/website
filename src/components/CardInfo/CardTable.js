307d1b1e723b5669848afaa04fe5938dee5dc21b
Admin <channingwang2018@gmail.com>
2023-09-25 02:03:45 +0800

    test ComplexTable component

--- src/components/CardInfo/CardTable.js
+++ src/components/CardInfo/CardTable.js
@@ -1,6 +1,5 @@
 import React, { memo, useEffect, useState } from 'react';
 import { usePluginData } from '@docusaurus/useGlobalData';
-import {Highlight} from './fontStyle'
 
 const CardStaticTable = memo(({cardKey}) => {
   const [data, setData] = useState(null);
@@ -13,11 +12,75 @@ const CardStaticTable = memo(({cardKey}) => {
     }
   }, [pluginData, cardKey]);
 
+  if (!data) {
+    return null; // or some loading state
+  }
+
+  const staticData = Object.entries(data.static_attributes)
+
   return (  
-    <div>
-      
-    </div>
+    <table>
+      <thead>
+        <tr>
+          <td></td>
+          <td><strong>固定数值</strong></td>
+        </tr>
+      </thead>
+      <tbody>
+        {staticData.map(([k, v]) => {return (
+          <tr>
+            <td><strong>{k}</strong></td>
+            <td>{v}</td>
+          </tr>            
+        )})}
+      </tbody>
+    </table>
   );
 });
 
-export default CardStaticTable;
+const CardComplexTable = memo(({cardKey}) => {
+  const [data, setData] = useState(null);
+  const pluginData = usePluginData("my-fetch-plugin");
+
+  useEffect(() => {
+    if (pluginData && pluginData.info) {
+      const cardData = pluginData.info[Number(cardKey)];
+      setData(cardData);
+    }
+  }, [pluginData, cardKey]);
+
+  if (!data) {
+    return null; // or some loading state
+  }
+
+  const complexData = data.growable_attributes
+  const keys = Object.keys(complexData[0])
+
+  return (  
+    <table>
+      <thead>
+        <tr>
+          {keys.map(k => {
+            return (
+              <td><strong>{k}</strong></td>
+            )
+          })}
+        </tr>
+      </thead>
+      <tbody>
+        {complexData.map((kv) => {return (
+          <tr>
+            {Object.values(kv).map(value => {return (
+              <td>
+                {value}
+              </td>
+            )
+            })}
+          </tr>            
+        )})}
+      </tbody>
+    </table>
+  );
+});
+
+export {CardStaticTable, CardComplexTable};
